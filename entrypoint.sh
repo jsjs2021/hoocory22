561d9d0536917078121dd97edc4baeed9c10f29f
jsjs2021 <83106937+jsjs2021@users.noreply.github.com>
2025-09-05 02:30:16 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'018e36c3-6aee-465a-81e0-be38a3318235'}
 WSPATH=${WSPATH:-'choreo'}
 
 # 生成配置文件，并经过 base64 加密
